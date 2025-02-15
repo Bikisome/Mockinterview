22e47051267ee7fd82ae28989aff128d2014291f
Bikram Ghoshal <bikramghoshal6337@gmail.com>
2025-02-15 15:45:46 +0530

    prject inititated

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-blue-400'>APP</div>
+      <div className='text-red-400'>APP</div>
+       
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
