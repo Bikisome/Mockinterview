b0cc656b2c4f59a4fa6bd62872018cb4b72f6313
Bikram Ghoshal <bikramghoshal6337@gmail.com>
2025-02-28 14:39:12 +0530

    added the authentication home page and all that stuffs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { cn } from "@/lib/utils"
+import { useAuth } from "@clerk/clerk-react"
+import { Navigate } from "react-router-dom"
+import  {Container} from "@/components/Container"
+import { Outlet } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import {LogoContainer} from "./LogoContainer"
+import {NavigationRoutes} from "./NavigationRoutes"
+import { ProfileContainer } from "./ProfileContainer"
+import { ToggleContainer } from "./ToggleContainer"
+
+
 
 const Header = () => {
+  const {userId} = useAuth();
+
+  
   return (
-    <div>Header</div>
+    <header className={cn("w-full border-b duration-150 transition-all ease")}>
+      <Container>
+      <div className="flex items-center gap-4 w-full">
+<LogoContainer/>
+
+<nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink
+                to={"/generate"}
+                className={({ isActive }) =>
+                  cn(
+                    "text-base text-neutral-600",
+                    isActive && "text-neutral-900 font-semibold"
+                  )
+                }
+              >
+                Take An Interview
+              </NavLink>
+            )}
+          </nav>
+
+          <div className="ml-auto flex items-center gap-6">
+          
+            <ProfileContainer />
+
+         
+            <ToggleContainer />
+          </div>
+        </div>
+      </Container>
+
+    </header>
   )
 }
 
--- src/layouts/PublickLayout.tsx
+++ src/layouts/PublickLayout.tsx
@@ -1,13 +1,16 @@
 
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import  {Footer } from "@/components/Footer";
 import { Outlet } from "react-router-dom";
+import AuthHanlder from "@/handlers/AuthHandler";
 
 
 export const PublicLayout = () => {
   return (
     <div className="w-full">
       {/* handler to store the user data */}
+
+      <AuthHanlder/>
 
       <Header />
 
